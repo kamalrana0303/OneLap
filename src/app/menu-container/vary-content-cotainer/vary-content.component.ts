0b6bb932d5697237bfbb5923320953d35708c0dd
kamalrana0303 <kamalrana03@gmail.com>
2021-01-03 18:20:45 +0530

    third commit

--- src/app/menu-container/vary-content-cotainer/vary-content.component.ts
+++ src/app/menu-container/vary-content-cotainer/vary-content.component.ts
@@ -6,18 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vary-content.component.css']
 })
 export class VaryContentComponent implements OnInit {
-  slide="./assets/1.png";
+  
+
   constructor() { }
 
   ngOnInit(): void {
-    setInterval(()=>{
-      const imgsrc=this.slide.substring(this.slide.lastIndexOf('/')+1, this.slide.lastIndexOf('/')+2);
-      let index=(+imgsrc)+1;
-      if(index>7){
-        index=1;
-      }
-      this.slide="./assets/"+index+".png"
-    },3000);
   }
 
 }
